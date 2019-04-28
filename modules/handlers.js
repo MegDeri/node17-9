c5bda3ca8f1bfd757839211878361d143863067d
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-04-28 17:54:37 +0300

    Modify modules and package

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,13 +1,38 @@
+var fs = require('fs');
+var formidable = require('formidable');
+
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsluge zadania upload.");
-    response.write("Rozpoczynam upload!");
-    response.end();
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+        fs.renameSync(files.upload.path, "test.png");
+        /*fs.readFile(files.upload.path, function(err,content) {
+			fs.writeFile('./test.png', content, function(){
+				console.log('The file has been saved!');
+			});
+		});*/
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
 exports.welcome = function(request, response) {
     console.log("Rozpoczynam obsluge zadania welcome.");
-    response.write("Witaj na stronie startowej!");
+    fs.readFile('./templates/start.html', function(err, html){
+    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    response.write(html);
     response.end();
+});
+}
+
+exports.show = function(request, response){
+    fs.readFile("test.png", "binary", function(error, file) {
+        response.writeHead(200, {"Content-Type": "image/png"});
+        response.write(file, "binary");
+        response.end();
+    });
 }
 
 exports.error = function(request, response) {
